f9e0ab70aa887664e20921a9091c8117161b1169
Maximos Nikiforakis <nikiforos@live.co.uk>
2022-10-21 20:17:36 +0300

    more interp tests

--- intro/test/interp_test.c
+++ intro/test/interp_test.c
@@ -9,3 +9,39 @@ Test(interp_test, prog1)
     table h = interp(prog);
     cr_assert(get("a", h) == 12, "!");
 }
+
+Test(interp_test, prog2)
+{
+    // a := 5 ; b := a + 2 ; c := b - 6 
+    stm prog = CompoundStm(
+        AssignStm(String("a"), NumExp(5)),
+        CompoundStm(
+            AssignStm(String("b"), OpExp(IdExp(String("a")), plus, NumExp(2))),
+            AssignStm(String("c"), OpExp(IdExp(String("b")), minus, NumExp(6)))
+        )
+    );
+    table h = interp(prog);
+    cr_assert(get("a", h) == 5);
+    cr_assert(get("b", h) == 7);
+    cr_assert(get("c", h) == 1);
+}
+
+Test(interp_test, prog3)
+{
+    // _var := ( a := 1, a := a + 1 ; a := a + 1, a * a)
+    stm prog = AssignStm(
+        String("a"),
+        EseqExp(
+            CompoundStm(
+                AssignStm(String("a"), NumExp(1)),
+                CompoundStm(
+                    AssignStm(String("a"), OpExp(IdExp(String("a")), plus, NumExp(1))),
+                    AssignStm(String("a"), OpExp(IdExp(String("a")), plus, NumExp(1)))
+                    )
+                ),
+                OpExp(IdExp(String("a")), times, IdExp(String("a")))
+            )
+        );
+    table h = interp(prog);
+    cr_assert(get("a", h) == 9);
+}
